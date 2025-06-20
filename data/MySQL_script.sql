81d92fec2999adb42e494c42cb22e584e27131a3
Ashwin k k <96005336+ashwinkk12345@users.noreply.github.com>
2025-06-20 20:42:17 +0530

    Update MySQL_script.sql

--- data/MySQL_script.sql
+++ data/MySQL_script.sql
@@ -1,6 +1,6 @@
--- execute this before running mysql_data_dumper.py
+-- Execute this before running mysql_data_dumper.py
 CREATE DATABASE IF NOT EXISTS churn_db;
+-- run mysql_data_dumper.py 
 
--- execute this after running mysql_data_dumper.py and creating database.
 USE churn_db;
 select * from customer_churn;
